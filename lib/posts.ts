64754f0f0d62be6b428fdc29b6c382da1aca84ca
AmirW <eng.amir.bu@gmail.com>
2023-03-21 20:39:43 +0330

    missing title query

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,6 +6,7 @@ export async function getByPath(path: string) {
       query: `
 			query {
 				page(urlPath: "${path}") {
+					title
 					... on SimplePage {
 						body {
 							... on RichTextBlock {
